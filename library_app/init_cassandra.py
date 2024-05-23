4ffbd3dff56f7863b425cb5c27e8a0fa3aff8a02
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-05-23 16:05:40 +0200

    Insert with LWT. Refactor init_cassandra

--- library_app/init_cassandra.py
+++ library_app/init_cassandra.py
@@ -1,6 +1,12 @@
+# type: ignore
 import os
 
-from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT, ConsistencyLevel # type: ignore
+from cassandra.cluster import (
+    Cluster,
+    ExecutionProfile,
+    EXEC_PROFILE_DEFAULT,
+    ConsistencyLevel,
+)
 
 
 KEYSPACE_NAME = os.environ.get("KEYSPACE_NAME", "library")
@@ -8,23 +14,25 @@ KEYSPACE_NAME = os.environ.get("KEYSPACE_NAME", "library")
 
 def main():
     exec_profiles = {
-        EXEC_PROFILE_DEFAULT: ExecutionProfile(consistency_level=ConsistencyLevel.QUORUM, request_timeout=50)
+        EXEC_PROFILE_DEFAULT: ExecutionProfile(
+            consistency_level=ConsistencyLevel.QUORUM, request_timeout=50
+        )
     }
 
     cluster = Cluster(["127.0.0.1"], port=9042, execution_profiles=exec_profiles)
     session = cluster.connect()
 
-    session.execute(f"DROP KEYSPACE IF EXISTS {KEYSPACE_NAME};")
-    session.execute(
+    queries = [
+        f"DROP KEYSPACE IF EXISTS {KEYSPACE_NAME};",
         f"CREATE KEYSPACE IF NOT EXISTS {KEYSPACE_NAME} "
-        "WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-    )
-
-    session.execute(
+        "WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};",
         f"CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.reservations "
-        "(reservation_id uuid, book_id int, customer_id int, reservation_date timestamp, PRIMARY KEY(book_id, customer_id));"
-    )
-    session.execute(f"TRUNCATE TABLE {KEYSPACE_NAME}.reservations;")
+        "(reservation_id uuid, book_id int, customer_id int, reservation_date timestamp, PRIMARY KEY(book_id));",
+        f"TRUNCATE TABLE {KEYSPACE_NAME}.reservations;",
+    ]
+
+    for query in queries:
+        session.execute(query)
 
 
 if __name__ == "__main__":
--- library_app/server.py
+++ library_app/server.py
@@ -2,7 +2,6 @@ import tornado.ioloop
 import tornado.web
 import uuid
 from cassandra.cluster import Cluster, Session  # type: ignore
-from cassandra.query import SimpleStatement  # type: ignore
 from datetime import datetime
 
 
@@ -19,13 +18,17 @@ class MakeReservationHandler(BaseHandler):
         reservation_date = int(datetime.now().timestamp() * 1000)
 
         query = (
-            "INSERT INTO reservations (reservation_id, book_id, customer_id, reservation_date) "
-            "VALUES (%s, %s, %s, %s);"
+            "INSERT INTO reservations (book_id, customer_id, reservation_date, reservation_id) "
+            "VALUES (%s, %s, %s, %s)"
+            "IF NOT EXISTS;"
         )
-        self.cassandra_session.execute(
-            query, (reservation_id, book_id, customer_id, reservation_date)
-        )
-        self.write({"status": "success", "reservation_id": str(reservation_id)})
+        result = self.cassandra_session.execute(
+            query, (book_id, customer_id, reservation_date, reservation_id)
+        ).one()
+        if result.applied:
+            self.write({"status": "success", "reservation_id": str(reservation_id)})
+        else:
+            self.write({"status": "already_reserved"})
 
 
 class UpdateReservationHandler(BaseHandler):
@@ -33,9 +36,11 @@ class UpdateReservationHandler(BaseHandler):
         book_id = int(self.get_argument("book_id"))
         customer_id = int(self.get_argument("customer_id"))
         reservation_date = datetime.now().timestamp() * 1000
-        
+
         query = "UPDATE reservations SET reservation_date = %s WHERE book_id = %s and customer_id = %s;"
-        self.cassandra_session.execute(query, (int(reservation_date), book_id, customer_id))
+        self.cassandra_session.execute(
+            query, (int(reservation_date), book_id, customer_id)
+        )
         self.write({"status": "success", "reservation_date": reservation_date})
 
 
@@ -59,7 +64,24 @@ class ViewReservationHandler(BaseHandler):
             self.write({"status": "not found"})
 
 
-def make_app(cassandra_session):
+class ListReservationHandler(BaseHandler):
+    async def get(self):
+        query = "SELECT * FROM reservations;"
+        reservations = self.cassandra_session.execute(query)
+        result = []
+        for reservation in reservations:
+            result.append(
+                {
+                    "reservation_id": str(reservation.reservation_id),
+                    "book_id": reservation.book_id,
+                    "customer_id": reservation.customer_id,
+                    "reservation_date": str(reservation.reservation_date),
+                }
+            )
+        self.write({"reservations": result})
+
+
+def make_app(cassandra_session, debug=False):
     return tornado.web.Application(
         [
             (
@@ -77,15 +99,23 @@ def make_app(cassandra_session):
                 ViewReservationHandler,
                 dict(cassandra_session=cassandra_session),
             ),
-        ]
+            (
+                r"/list_reservations",
+                ListReservationHandler,
+                dict(cassandra_session=cassandra_session),
+            ),
+        ],
+        debug=debug,
     )
 
 
 if __name__ == "__main__":
-    cluster = Cluster(["127.0.0.1"], port=9042)  # Replace with your Cassandra cluster nodes
+    cluster = Cluster(
+        ["127.0.0.1"], port=9042
+    )  # Replace with your Cassandra cluster nodes
     session = cluster.connect("library")
 
-    app = make_app(session)
+    app = make_app(session, debug=False)
     app.listen(8888)
     print("Started listening...")
     tornado.ioloop.IOLoop.current().start()
