dc0d4636cbb7707a114e5651c98b8c0ebbde50ba
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-05-25 22:17:26 +0200

    feat(fastapi): 2 nodes with ensured consistency; switch to fastapi

--- library_app/init_cassandra.py
+++ library_app/init_cassandra.py
@@ -1,38 +1,82 @@
 # type: ignore
 import os
 
+from cassandra.query import BatchStatement
 from cassandra.cluster import (
     Cluster,
+    Session,
     ExecutionProfile,
     EXEC_PROFILE_DEFAULT,
     ConsistencyLevel,
 )
+import time
 
 
 KEYSPACE_NAME = os.environ.get("KEYSPACE_NAME", "library")
 
 
+def get_schema_version(session: Session):
+    query = "SELECT schema_version FROM system.local;"
+    result = session.execute(query).one()
+    return result.schema_version
+
+
+def ensure_schemas_equality(nodes: list[str], verbose: bool = False):
+    clusters = [Cluster([node]) for node in nodes]
+    sessions = [cluster.connect() for cluster in clusters]
+
+    while True:
+        schema_versions = []
+        for session in sessions:
+            schema_version = get_schema_version(session)
+            schema_versions.append(schema_version)
+
+        if verbose:
+            print(f"{schema_versions = }")
+        if len(set(schema_versions)) == 1:
+            break
+        time.sleep(1)
+
+    for session, cluster in zip(sessions, clusters):
+        session.shutdown()
+        cluster.shutdown()
+
+
 def main():
     exec_profiles = {
         EXEC_PROFILE_DEFAULT: ExecutionProfile(
-            consistency_level=ConsistencyLevel.QUORUM, request_timeout=50
+            consistency_level=ConsistencyLevel.ALL, request_timeout=200
         )
     }
 
-    cluster = Cluster(["127.0.0.1"], port=9042, execution_profiles=exec_profiles)
+    nodes = ["127.0.0.1", "127.0.0.2"]
+    cluster = Cluster(nodes, port=9042, execution_profiles=exec_profiles)
     session = cluster.connect()
 
     queries = [
         f"DROP KEYSPACE IF EXISTS {KEYSPACE_NAME};",
         f"CREATE KEYSPACE IF NOT EXISTS {KEYSPACE_NAME} "
-        "WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};",
+        "WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2};",
+        f"CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.books "
+        "(book_id int, title text, PRIMARY KEY(book_id));",
         f"CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.reservations "
-        "(reservation_id uuid, book_id int, customer_id int, reservation_date timestamp, PRIMARY KEY(book_id));",
-        f"TRUNCATE TABLE {KEYSPACE_NAME}.reservations;",
+        "(book_id int, customer_id int, reservation_id uuid, reservation_date timestamp, PRIMARY KEY(book_id));",
+        f"CREATE TABLE IF NOT EXISTS {KEYSPACE_NAME}.customers "
+        "(customer_id int, num_reservations int, PRIMARY KEY(customer_id, num_reservations));",
     ]
 
     for query in queries:
         session.execute(query)
+
+    ensure_schemas_equality(nodes, verbose=False)
+
+    insert = BatchStatement()
+    for i in range(100):
+        insert.add(
+            f"INSERT INTO {KEYSPACE_NAME}.books (book_id, title) VALUES (%s, 'Lolkek');",
+            (i,),
+        )
+    session.execute(insert)
 
 
 if __name__ == "__main__":
--- library_app/server.py
+++ library_app/server.py
@@ -1,11 +1,12 @@
-import tornado.ioloop
-import tornado.web
-import uuid
-from cassandra.query import PreparedStatement  # type: ignore
-from cassandra.cluster import Cluster, Session  # type: ignore
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+from uuid import uuid4
 from datetime import datetime
-import time
 from dataclasses import dataclass
+
+from cassandra.cluster import Cluster  # type: ignore
+from cassandra.query import PreparedStatement  # type: ignore
 
 
 @dataclass
@@ -14,145 +15,78 @@ class PreparedStatements:
     insert_reservation: PreparedStatement
 
 
-class BaseHandler(tornado.web.RequestHandler):
-    def initialize(
-        self, cassandra_session: Session, prepared_statements: PreparedStatements
-    ):
-        self.cassandra_session = cassandra_session
-        self.prepared_statements = prepared_statements
+RESERVATION_PER_USER_LIMIT = 100
+
+cluster = Cluster(["127.0.0.1", "127.0.0.2"], port=9042)
+db = cluster.connect("library")
+
+prep_statements = PreparedStatements(
+    select_reservation=db.prepare("SELECT * FROM reservations WHERE book_id = ?;"),
+    insert_reservation=db.prepare(
+        "INSERT INTO reservations (book_id, customer_id, reservation_date, reservation_id) "
+        "VALUES (?, ?, ?, ?) "
+        "IF NOT EXISTS;"
+    ),
+)
 
 
-class MakeReservationHandler(BaseHandler):
-    async def post(self):
-        book_id = int(self.get_argument("book_id"))
-        customer_id = int(self.get_argument("customer_id"))
-        reservation_id = uuid.uuid4()
-        reservation_date = int(datetime.now().timestamp() * 1000)
-
-        future = self.cassandra_session.execute_async(
-            self.prepared_statements.insert_reservation,
-            (book_id, customer_id, reservation_date, reservation_id),
-        )
-
-        result = future.result().one()
-        if result.applied:
-            self.write({"status": "success", "reservation_id": str(reservation_id)})
-        else:
-            raise tornado.web.HTTPError(400, "Book already resered")
+app = FastAPI()
 
 
-class UpdateReservationHandler(BaseHandler):
-    async def post(self):
-        # book_id = int(self.get_argument("book_id"))
-        # customer_id = int(self.get_argument("customer_id"))
-        # reservation_date = datetime.now().timestamp() * 1000
+@app.post("/make_reservation")
+async def make_reservation(book_id: int, customer_id: int):
+    reservation_id = uuid4()
+    reservation_date = int(datetime.now().timestamp() * 1000)
 
-        # query = "UPDATE reservations SET reservation_date = %s WHERE book_id = %s and customer_id = %s;"
-        # self.cassandra_session.execute(
-        #     query, (int(reservation_date), book_id, customer_id)
-        # )
-        # self.write({"status": "success", "reservation_date": reservation_date})
-        time.sleep(10)
-        self.write({"status": "success"})
-
-
-class ViewReservationHandler(BaseHandler):
-    async def get(self):
-        book_id = int(self.get_argument("book_id"))
-
-        future = self.cassandra_session.execute_async(
-            self.prepared_statements.select_reservation, (book_id,)
-        )
-        result = future.result().one()
-
-        if result:
-            self.write(
-                {
-                    "reservation_id": str(result.reservation_id),
-                    "book_id": result.book_id,
-                    "customer_id": result.customer_id,
-                    "reservation_date": str(result.reservation_date),
-                }
-            )
-        else:
-            raise tornado.web.HTTPError(400, "Book not found")
-
-
-class ListReservationHandler(BaseHandler):
-    async def get(self):
-        query = "SELECT * FROM reservations;"
-
-        future = self.cassandra_session.execute_async(query)
-        result = future.result().all()
-        reservations = []
-        for reservation in result:
-            reservations.append(
-                {
-                    "reservation_id": str(reservation.reservation_id),
-                    "book_id": reservation.book_id,
-                    "customer_id": reservation.customer_id,
-                    "reservation_date": str(reservation.reservation_date),
-                }
-            )
-        self.write({"reservations": reservations})
-
-
-def make_app(cassandra_session, prepared_statements, debug=False):
-    return tornado.web.Application(
-        [
-            (
-                r"/make_reservation",
-                MakeReservationHandler,
-                dict(
-                    cassandra_session=cassandra_session,
-                    prepared_statements=prepared_statements,
-                ),
-            ),
-            (
-                r"/update_reservation",
-                UpdateReservationHandler,
-                dict(
-                    cassandra_session=cassandra_session,
-                    prepared_statements=prepared_statements,
-                ),
-            ),
-            (
-                r"/view_reservation",
-                ViewReservationHandler,
-                dict(
-                    cassandra_session=cassandra_session,
-                    prepared_statements=prepared_statements,
-                ),
-            ),
-            (
-                r"/list_reservations",
-                ListReservationHandler,
-                dict(
-                    cassandra_session=cassandra_session,
-                    prepared_statements=prepared_statements,
-                ),
-            ),
-        ],
-        debug=debug,
+    future = db.execute_async(
+        prep_statements.insert_reservation,
+        (book_id, customer_id, reservation_date, reservation_id),
     )
 
+    result = future.result().one()
+    if result.applied:
+        return {"status": "success", "reservation_id": str(reservation_id)}
+    else:
+        raise HTTPException(400, "Book already reserved")
 
-if __name__ == "__main__":
-    cluster = Cluster(["127.0.0.1"], port=9042)
-    session = cluster.connect("library")
 
-    prepared_statements = PreparedStatements(
-        select_reservation=session.prepare(
-            "SELECT * FROM reservations WHERE book_id = ?;"
-        ),
-        insert_reservation=session.prepare(
-            "INSERT INTO reservations (book_id, customer_id, reservation_date, reservation_id) "
-            "VALUES (?, ?, ?, ?)"
-            "IF NOT EXISTS;"
-        ),
-    )
+@app.get("/view_reservation")
+async def view_reservation(book_id: int):
+    future = db.execute_async(prep_statements.select_reservation, (book_id,))
+    result = future.result().one()
 
-    app = make_app(session, prepared_statements, debug=False)
-    app.listen(8888)
-    print("Started listening...")
-    tornado.ioloop.IOLoop.current().start()
+    if result:
+        return {
+            "reservation_id": str(result.reservation_id),
+            "book_id": result.book_id,
+            "customer_id": result.customer_id,
+            "reservation_date": str(result.reservation_date),
+        }
+    else:
+        raise HTTPException(400, f"No reservation for book with book_id={book_id}")
+
+
+@app.get("/list_reservations")
+async def list_reservations():
+    future = db.execute_async("SELECT * FROM reservations;")
+    result = future.result().all()
+    reservations = []
+    for reservation in result:
+        reservations.append(
+            {
+                "reservation_id": str(reservation.reservation_id),
+                "book_id": reservation.book_id,
+                "customer_id": reservation.customer_id,
+                "reservation_date": str(reservation.reservation_date),
+            }
+        )
+    return {"reservations": reservations}
+
+
+@app.post("/clear")
+async def clear():  # db: Session = Depends(get_db)):
+    db.execute("TRUNCATE TABLE reservations;")
+    db.execute("TRUNCATE TABLE customers;")
+
+
+uvicorn.run(app, port=8888, log_level="info")
