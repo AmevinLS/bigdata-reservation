02efa240b42b69c0de95bd1df518ea4979f562ab
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-05-29 23:01:59 +0200

    Feature: update_reservation endpoint; stress tests

--- library_app/server.py
+++ library_app/server.py
@@ -7,11 +7,24 @@ from datetime import datetime
 from cassandra.cluster import Cluster  # type: ignore
 
 
-RESERVATION_PER_USER_LIMIT = 100
+RESERVATION_PER_USER_LIMIT = 500
 
 cluster = Cluster(["127.0.0.1", "127.0.0.2"], port=9042)
 db = cluster.connect("library")
 
+select_count_reservations = db.prepare(
+    "SELECT COUNT(*) FROM reservations_by_customer_id WHERE customer_id = ?;"
+)
+insert_reservation_by_book_id = db.prepare(
+    "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) "
+    "VALUES (?, ?, ?, ?) IF NOT EXISTS;",
+)
+insert_reservation_by_id = db.prepare(
+    "INSERT INTO reservations_by_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
+)
+insert_reservation_by_customer_id = db.prepare(
+    "INSERT INTO reservations_by_customer_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
+)
 
 app = FastAPI()
 
@@ -19,57 +32,118 @@ app = FastAPI()
 @app.post("/make_reservation")
 async def make_reservation(book_id: int, customer_id: int):
     reservation_id = uuid4()
-    reservation_date = int(datetime.now().timestamp() * 1000)
+    reservation_date_obj = datetime.now()
+    reservation_date = int(reservation_date_obj.timestamp() * 1000)
 
-    # Insert LWT
-    # future = db.execute_async(
-    #     prep_statements.insert_reservation_lwt,
-    #     (book_id, customer_id, reservation_date, reservation_id),
-    # )
-    # result = future.result().one()
-    # if not result.applied:
-    #     # future = db.execute_async(prep_statements.decrement_customer)
-    #     # result = future.result().one()  # Possible comment this to increase performance
-    #     raise HTTPException(400, "Book already reserved.")
-
-    # Check if reservation already exists for book
-    prev_reservs = (
+    count_result = (
         db.execute_async(
-            "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
-        )
-        .result()
-        .all()
-    )
-    if len(prev_reservs) > 0:
-        raise HTTPException(400, "Book already reserved")
-
-    # Insert reservation
-    db.execute_async(
-        "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) VALUES (%s, %s, %s, %s);",
-        (book_id, customer_id, reservation_id, reservation_date),
-    ).result()
-
-    # Check if the reservation in the table is ours
-    new_reserv = (
-        db.execute_async(
-            "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
+            select_count_reservations,
+            (customer_id,),
         )
         .result()
         .one()
     )
-    if new_reserv.reservation_id != reservation_id:
-        raise HTTPException(400, "Book already reserved")
+    if count_result.count >= RESERVATION_PER_USER_LIMIT:
+        raise HTTPException(401, "Reservation limit reached")
+
+    # Insert LWT
+    result = (
+        db.execute_async(
+            insert_reservation_by_book_id,
+            (book_id, customer_id, reservation_id, reservation_date),
+        )
+        .result()
+        .one()
+    )
+    if not result.applied:
+        raise HTTPException(400, "Book already reserved.")
+
+    # Check if reservation already exists for book
+    # prev_reservs = (
+    #     db.execute_async(
+    #         "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
+    #     )
+    #     .result()
+    #     .all()
+    # )
+    # if len(prev_reservs) > 0:
+    #     raise HTTPException(400, "Book already reserved")
+
+    # # Insert reservation
+    # db.execute_async(
+    #     "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) VALUES (%s, %s, %s, %s);",
+    #     (book_id, customer_id, reservation_id, reservation_date),
+    # ).result()
+
+    # # Check if the reservation in the table is ours
+    # new_reserv = None
+    # while new_reserv is None:
+    #     new_reserv = (
+    #         db.execute_async(
+    #             "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
+    #         )
+    #         .result()
+    #         .one()
+    #     )
+    # if new_reserv.reservation_id != reservation_id:
+    #     raise HTTPException(400, "Book already reserved")
 
     db.execute_async(
-        "INSERT INTO reservations_by_id (book_id, customer_id, reservation_id, reservation_date) VALUES (%s, %s, %s, %s);",
+        insert_reservation_by_id,
         (book_id, customer_id, reservation_id, reservation_date),
-    ).result()
+    )
     db.execute_async(
-        "INSERT INTO reservations_by_customer_id (book_id, customer_id, reservation_id, reservation_date) VALUES (%s, %s, %s, %s);",
+        insert_reservation_by_customer_id,
         (book_id, customer_id, reservation_id, reservation_date),
-    ).result()
+    )
 
     return {"status": "success", "reservation_id": str(reservation_id)}
+
+
+@app.post("/update_reservation")
+async def update_reservation(book_id: int, customer_id: int):
+    reservation_date_obj = datetime.now()
+    reservation_date = int(reservation_date_obj.timestamp() * 1000)
+
+    result = (
+        db.execute_async(
+            "UPDATE reservations_by_customer_id SET reservation_date = %s "
+            "WHERE book_id = %s AND customer_id = %s IF reservation_date < %s;",
+            (reservation_date, book_id, customer_id, reservation_date),
+        )
+        .result()
+        .one()
+    )
+    if not result.applied:
+        raise HTTPException(
+            402, "Cannot update reservation with earlier 'reservation_date'"
+        )
+
+    reserv_result = (
+        db.execute_async(
+            "SELECT reservation_id FROM reservations_by_customer_id "
+            "WHERE customer_id = %s AND book_id = %s;",
+            (customer_id, book_id),
+        )
+        .result()
+        .one()
+    )
+
+    db.execute_async(
+        "UPDATE reservations_by_book_id SET reservation_date = %s "
+        "WHERE book_id = %s;",
+        (reservation_date, book_id),
+    )
+    db.execute_async(
+        "UPDATE reservations_by_id SET reservation_date = %s "
+        "WHERE reservation_id = %s;",
+        (reservation_date, reserv_result.reservation_id),
+    )
+
+    return {
+        "reservation_id": reserv_result.reservation_id,
+        "reservation_date": reservation_date_obj,
+    }
 
 
 @app.get("/view_reservation")
@@ -121,7 +195,7 @@ async def list_reservations(customer_id: int | None = None):
 
 
 @app.post("/clear")
-async def clear():  # db: Session = Depends(get_db)):
+async def clear():
     db.execute("TRUNCATE TABLE reservations_by_book_id;")
     db.execute("TRUNCATE TABLE reservations_by_id;")
     db.execute("TRUNCATE TABLE reservations_by_customer_id;")
--- load_testing/autocannon_test.js
+++ load_testing/autocannon_test.js
@@ -2,35 +2,130 @@ const autocannon = require("autocannon");
 const axios = require("axios");
 
 
-function setupViewRequest(request, context) {
-    const book_id = Math.floor(Math.random() * 10);
-    request.path = `/view_reservation?book_id=${book_id}`;
-    return request;
-}
-
-async function testViewReservation() {
+async function testOneCustomerSameRequest(request, num_requests) {
     const result = await autocannon({
-        url: "http://localhost:8888",
+        url: "http://localhost:8888/view_reservation?book_id=0",
         connections: 1,
         pipelining: 1,
         duration: 0,
-        amount: 10000,
+        amount: num_requests,
         requests: [
-            {
-                method: 'GET',
-                setupRequest: setupViewRequest
-            }
+            request
         ]
     });
     console.log(result);
 }
 
 
-function createMakeRequests(customer_id, num_books) {
+function randomViewRequest(num_books) {
+    const book_id = Math.floor(Math.random() * num_books); 
+    return {
+        method: "GET",
+        path: `/view_reservation?book_id=${book_id}`
+    };
+}
+
+function randomMakeRequest(customer_id, num_books) {
+    const book_id = Math.floor(Math.random() * num_books);
+    return {
+        method: "POST",
+        path: `/make_reservation?customer_id=${customer_id}&book_id=${book_id}`
+    };
+}
+
+function randomUpdateRequest(customer_id, num_books) {
+    const book_id = Math.floor(Math.random() * num_books);
+    return {
+        method: "POST",
+        path: `/update_reservation?book_id=${book_id}&customer_id=${customer_id}`
+    };
+}
+
+function randomListRequest(customer_id) {
+    if (Math.random() < 0.5) {
+        return {
+            method: "GET",
+            path: `/list_reservations?customer_id=${customer_id}`
+        };
+    }
+    else {
+        return {
+            method: "GET",
+            path: `/list_reservations`
+        }
+    }
+    
+}
+
+function setupRandomRequest(request, context) {
+    const request_ind = Math.floor(Math.random() * 4);
+    let temp_req = null;
+    switch (request_ind) {
+        case 0:
+            temp_req = randomViewRequest(context.num_books);
+            break;
+        case 1:
+            temp_req = randomMakeRequest(context.customer_id, context.num_books);
+            break;
+        case 2:
+            temp_req = randomUpdateRequest(context.customer_id, context.num_books);
+            break;
+        case 3:
+            temp_req = randomListRequest(context.customer_id);
+            break;
+        default:
+            throw Error("This isn't supposed to happen");
+    }
+    request.method = temp_req.method;
+    request.path = temp_req.path;
+    return request;
+}
+
+async function testTwoCustomersRandomRequests(num_books, num_requests_each) {
+    console.log("Started two-customer random requests stress test...");
+    const promise1 = autocannon({
+        url: "http://localhost:8888",
+        connections: 1,
+        pipelining: 1,
+        duration: 0,
+        amount: num_requests_each,
+        initialContext: {
+            num_books: num_books,
+            customer_id: 0
+        },
+        requests: [
+            {
+                setupRequest: setupRandomRequest
+            }
+        ]
+    });
+    const promise2 = autocannon({
+        url: "http://localhost:8888",
+        connections: 1,
+        pipelining: 1,
+        duration: 0,
+        amount: num_requests_each,
+        initialContext: {
+            num_books: num_books,
+            customer_id: 1
+        },
+        requests: [
+            {
+                setupRequest: setupRandomRequest
+            }
+        ]
+    });
+
+    const results1 = await promise1;
+    const results2 = await promise2;
+    console.log("RESULTS 1:", results1);
+    console.log("RESULTS 2:", results2);
+}
+
+
+function createConsecutiveMakeRequests(customer_id, num_books) {
     const requests = [...Array(num_books).keys()].map(book_id => {
         return {
-            // method: "GET",
-            // path: `/view_reservation?book_id=${book_id}`
             method: "POST",
             path: `/make_reservation?book_id=${book_id}&customer_id=${customer_id}`
         }
@@ -38,10 +133,11 @@ function createMakeRequests(customer_id, num_books) {
     return requests;
 }
 
-async function testTwoCustomerReservations(num_books) {
-    const requests1 = createMakeRequests(0, num_books);
-    const requests2 = createMakeRequests(1, num_books);
+async function testTwoCustomersReservations(num_books) {
+    const requests1 = createConsecutiveMakeRequests(0, num_books);
+    const requests2 = createConsecutiveMakeRequests(1, num_books);
 
+    console.log("Started two-customer reservations stress test...");
     const promise1 = autocannon({
         url: "http://localhost:8888",
         connections: 1,
@@ -66,4 +162,21 @@ async function testTwoCustomerReservations(num_books) {
 }
 
 
-testTwoCustomerReservations(1000);
+const num_books = 1000;
+
+// Stress test 1
+const num_requests = 1000;
+testOneCustomerSameRequest(
+    {
+        method: "GET",
+        path: "/view_reservation?book_id=0"
+    },
+    num_requests
+)
+
+// Stress test 2
+// const num_requests_each = 1000;
+// testTwoCustomersRandomRequests(num_books, num_requests_each);
+
+// Stress test 3
+// testTwoCustomersReservations(num_books);
