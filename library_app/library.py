d9c49c6e27d94dc0ed91aad61d0849a6d71dfc6d
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-06-05 23:38:39 +0200

    Refactor: move Cassandra logic into separate module

---
+++ library_app/library.py
@@ -0,0 +1,162 @@
+from uuid import UUID
+from cassandra.cluster import Cluster  # type: ignore
+
+
+class LibraryWrapper:
+    def __init__(self):
+        self.cluster = Cluster(["127.0.0.1", "127.0.0.2"], port=9042)
+        self.db = self.cluster.connect("library")
+
+        self.select_count_reservations = self.db.prepare(
+            "SELECT COUNT(*) FROM reservations_by_customer_id WHERE customer_id = ?;"
+        )
+        self.select_reservation_by_book_id = self.db.prepare(
+            "SELECT * FROM reservations_by_book_id WHERE book_id = ?;"
+        )
+        self.insert_reservation_by_book_id = self.db.prepare(
+            "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) "
+            "VALUES (?, ?, ?, ?) IF NOT EXISTS;",
+        )
+        self.insert_reservation_by_id = self.db.prepare(
+            "INSERT INTO reservations_by_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
+        )
+        self.insert_reservation_by_customer_id = self.db.prepare(
+            "INSERT INTO reservations_by_customer_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
+        )
+        self.select_reservation_by_customer_id = self.db.prepare(
+            "SELECT * FROM reservations_by_customer_id WHERE customer_id = ?;"
+        )
+        self.select_all_reservations = self.db.prepare(
+            "SELECT * FROM reservations_by_customer_id;"
+        )
+
+    async def get_books(self, only_available: bool = False):
+        if only_available:
+            books = self.db.execute_async()
+        else:
+            books = self.db.execute_async("SELECT * FROM books;").result().all()
+        return books
+
+    async def get_reservation_count(self, customer_id: int) -> int:
+        count = (
+            self.db.execute_async(
+                self.select_count_reservations,
+                (customer_id,),
+            )
+            .result()
+            .one()
+            .count
+        )
+        return count
+
+    async def insert_reservation(
+        self,
+        book_id: int,
+        customer_id: int,
+        reservation_id: UUID,
+        reservation_date: int,
+    ) -> bool:
+        applied = (
+            self.db.execute_async(
+                self.insert_reservation_by_book_id,
+                (book_id, customer_id, reservation_id, reservation_date),
+            )
+            .result()
+            .one()
+        ).applied
+
+        if applied:
+            self.db.execute_async(
+                self.insert_reservation_by_id,
+                (book_id, customer_id, reservation_id, reservation_date),
+            )
+            self.db.execute_async(
+                self.insert_reservation_by_customer_id,
+                (book_id, customer_id, reservation_id, reservation_date),
+            )
+
+        return applied
+
+    async def update_reservation(
+        self, reservation_date: int, book_id: int, customer_id: int
+    ) -> bool:
+        applied = (
+            self.db.execute_async(
+                "UPDATE reservations_by_customer_id SET reservation_date = %s "
+                "WHERE book_id = %s AND customer_id = %s IF reservation_date < %s;",
+                (reservation_date, book_id, customer_id, reservation_date),
+            )
+            .result()
+            .one()
+            .applied
+        )
+
+        if applied:
+            reserv_result = (
+                self.db.execute_async(
+                    "SELECT reservation_id FROM reservations_by_customer_id "
+                    "WHERE customer_id = %s AND book_id = %s;",
+                    (customer_id, book_id),
+                )
+                .result()
+                .one()
+            )
+
+            self.db.execute_async(
+                "UPDATE reservations_by_book_id SET reservation_date = %s "
+                "WHERE book_id = %s;",
+                (reservation_date, book_id),
+            )
+            self.db.execute_async(
+                "UPDATE reservations_by_id SET reservation_date = %s "
+                "WHERE reservation_id = %s;",
+                (reservation_date, reserv_result.reservation_id),
+            )
+
+        return applied
+
+    async def get_reservation(self, book_id: int):
+        result = (
+            self.db.execute_async(self.select_reservation_by_book_id, (book_id,))
+            .result()
+            .one()
+        )
+        if result is not None:
+            return {
+                "reservation_id": str(result.reservation_id),
+                "book_id": result.book_id,
+                "customer_id": result.customer_id,
+                "reservation_date": str(result.reservation_date),
+            }
+        else:
+            return None
+
+    async def get_reservations(self, customer_id: int | None):
+        if customer_id is not None:
+            result = (
+                self.db.execute_async(
+                    self.select_reservation_by_customer_id,
+                    (customer_id,),
+                )
+                .result()
+                .all()
+            )
+        else:
+            result = self.db.execute_async(self.select_all_reservations).result().all()
+        reservations = []
+        for reservation in result:
+            reservations.append(
+                {
+                    "reservation_id": str(reservation.reservation_id),
+                    "book_id": reservation.book_id,
+                    "customer_id": reservation.customer_id,
+                    "reservation_date": str(reservation.reservation_date),
+                }
+            )
+        return reservations
+
+    def clear_reservations(self):
+        timeout = 30
+        self.db.execute("TRUNCATE TABLE reservations_by_book_id;", timeout=timeout)
+        self.db.execute("TRUNCATE TABLE reservations_by_id;", timeout=timeout)
+        self.db.execute("TRUNCATE TABLE reservations_by_customer_id;", timeout=timeout)
--- library_app/server.py
+++ library_app/server.py
@@ -4,35 +4,12 @@ import uvicorn
 from uuid import uuid4
 from datetime import datetime
 
-from cassandra.cluster import Cluster  # type: ignore
+from library import LibraryWrapper
 
 
 RESERVATION_PER_USER_LIMIT = 500
 
-cluster = Cluster(["127.0.0.1", "127.0.0.2"], port=9042)
-db = cluster.connect("library")
-
-select_count_reservations = db.prepare(
-    "SELECT COUNT(*) FROM reservations_by_customer_id WHERE customer_id = ?;"
-)
-select_reservation_by_book_id = db.prepare(
-    "SELECT * FROM reservations_by_book_id WHERE book_id = ?;"
-)
-insert_reservation_by_book_id = db.prepare(
-    "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) "
-    "VALUES (?, ?, ?, ?) IF NOT EXISTS;",
-)
-insert_reservation_by_id = db.prepare(
-    "INSERT INTO reservations_by_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
-)
-insert_reservation_by_customer_id = db.prepare(
-    "INSERT INTO reservations_by_customer_id (book_id, customer_id, reservation_id, reservation_date) VALUES (?, ?, ?, ?);"
-)
-select_reservation_by_customer_id = db.prepare(
-    "SELECT * FROM reservations_by_customer_id WHERE customer_id = ?;"
-)
-select_all_reservations = db.prepare("SELECT * FROM reservations_by_customer_id;")
-
+library = LibraryWrapper()
 
 app = FastAPI()
 
@@ -43,67 +20,16 @@ async def make_reservation(book_id: int, customer_id: int):
     reservation_date_obj = datetime.now()
     reservation_date = int(reservation_date_obj.timestamp() * 1000)
 
-    count_result = (
-        db.execute_async(
-            select_count_reservations,
-            (customer_id,),
-        )
-        .result()
-        .one()
-    )
-    if count_result.count >= RESERVATION_PER_USER_LIMIT:
+    count_result = await library.get_reservation_count(customer_id)
+    if count_result >= RESERVATION_PER_USER_LIMIT:
         raise HTTPException(429, "Reservation limit reached")
 
     # Insert LWT
-    result = (
-        db.execute_async(
-            insert_reservation_by_book_id,
-            (book_id, customer_id, reservation_id, reservation_date),
-        )
-        .result()
-        .one()
+    applied = await library.insert_reservation(
+        book_id, customer_id, reservation_id, reservation_date
     )
-    if not result.applied:
+    if not applied:
         raise HTTPException(409, "Book already reserved.")
-
-    # Check if reservation already exists for book
-    # prev_reservs = (
-    #     db.execute_async(
-    #         "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
-    #     )
-    #     .result()
-    #     .all()
-    # )
-    # if len(prev_reservs) > 0:
-    #     raise HTTPException(400, "Book already reserved")
-
-    # # Insert reservation
-    # db.execute_async(
-    #     "INSERT INTO reservations_by_book_id (book_id, customer_id, reservation_id, reservation_date) VALUES (%s, %s, %s, %s);",
-    #     (book_id, customer_id, reservation_id, reservation_date),
-    # ).result()
-
-    # # Check if the reservation in the table is ours
-    # new_reserv = None
-    # while new_reserv is None:
-    #     new_reserv = (
-    #         db.execute_async(
-    #             "SELECT * FROM reservations_by_book_id WHERE book_id = %s;", (book_id,)
-    #         )
-    #         .result()
-    #         .one()
-    #     )
-    # if new_reserv.reservation_id != reservation_id:
-    #     raise HTTPException(400, "Book already reserved")
-
-    db.execute_async(
-        insert_reservation_by_id,
-        (book_id, customer_id, reservation_id, reservation_date),
-    )
-    db.execute_async(
-        insert_reservation_by_customer_id,
-        (book_id, customer_id, reservation_id, reservation_date),
-    )
 
     return {"status": "success", "reservation_id": str(reservation_id)}
 
@@ -113,95 +39,34 @@ async def update_reservation(book_id: int, customer_id: int):
     reservation_date_obj = datetime.now()
     reservation_date = int(reservation_date_obj.timestamp() * 1000)
 
-    result = (
-        db.execute_async(
-            "UPDATE reservations_by_customer_id SET reservation_date = %s "
-            "WHERE book_id = %s AND customer_id = %s IF reservation_date < %s;",
-            (reservation_date, book_id, customer_id, reservation_date),
-        )
-        .result()
-        .one()
-    )
-    if not result.applied:
+    applied = await library.update_reservation(reservation_date, book_id, customer_id)
+    if not applied:
         raise HTTPException(
             400, "Cannot update reservation with earlier 'reservation_date'"
         )
 
-    reserv_result = (
-        db.execute_async(
-            "SELECT reservation_id FROM reservations_by_customer_id "
-            "WHERE customer_id = %s AND book_id = %s;",
-            (customer_id, book_id),
-        )
-        .result()
-        .one()
-    )
-
-    db.execute_async(
-        "UPDATE reservations_by_book_id SET reservation_date = %s "
-        "WHERE book_id = %s;",
-        (reservation_date, book_id),
-    )
-    db.execute_async(
-        "UPDATE reservations_by_id SET reservation_date = %s "
-        "WHERE reservation_id = %s;",
-        (reservation_date, reserv_result.reservation_id),
-    )
-
     return {
-        "reservation_id": reserv_result.reservation_id,
         "reservation_date": reservation_date_obj,
     }
 
 
 @app.get("/view_reservation")
 async def view_reservation(book_id: int):
-    future = db.execute_async(select_reservation_by_book_id, (book_id,))
-    result = future.result().one()
-
-    if result:
-        return {
-            "reservation_id": str(result.reservation_id),
-            "book_id": result.book_id,
-            "customer_id": result.customer_id,
-            "reservation_date": str(result.reservation_date),
-        }
-    else:
+    result = await library.get_reservation(book_id)
+    if result is None:
         raise HTTPException(404, f"No reservation for book with book_id={book_id}")
+    return result
 
 
 @app.get("/list_reservations")
 async def list_reservations(customer_id: int | None = None):
-    if customer_id is not None:
-        result = (
-            db.execute_async(
-                select_reservation_by_customer_id,
-                (customer_id,),
-            )
-            .result()
-            .all()
-        )
-    else:
-        result = db.execute_async(select_all_reservations).result().all()
-    reservations = []
-    for reservation in result:
-        reservations.append(
-            {
-                "reservation_id": str(reservation.reservation_id),
-                "book_id": reservation.book_id,
-                "customer_id": reservation.customer_id,
-                "reservation_date": str(reservation.reservation_date),
-            }
-        )
+    reservations = await library.get_reservations(customer_id)
     return {"reservations": reservations}
 
 
 @app.post("/clear")
 async def clear():
-    timeout = 30
-    db.execute("TRUNCATE TABLE reservations_by_book_id;", timeout=timeout)
-    db.execute("TRUNCATE TABLE reservations_by_id;", timeout=timeout)
-    db.execute("TRUNCATE TABLE reservations_by_customer_id;", timeout=timeout)
+    library.clear_reservations()
     return Response(status_code=200, content="Successfully cleared tables")
 
 
